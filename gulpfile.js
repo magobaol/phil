da7924f35254a6b85f50b928d3393f16d1b7c584
Francesco Face <francesco.face@gmail.com>
2023-03-07 22:08:10 +0100

    Modify scripts to build a single js app and a single binary

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,19 @@ let uglify = require('gulp-uglify')
 let chmod = require('gulp-chmod')
 let insert = require('gulp-insert')
 let tsProject = ts.createProject('tsconfig.json')
+let concat = require('gulp-concat')
 
 function build() {
     return gulp.src('src/**/*.ts')
         .pipe(tsProject())
         //.pipe(uglify())
+        .pipe(concat('phil.js'))
         .pipe(gulp.dest('dist'))
 }
 
 function release() {
-    return gulp.src('dist/index.js')
-        .pipe(insert.prepend(`#!/usr/bin/env node
-`))
+    return gulp.src('dist/phil.js')
+        .pipe(insert.prepend(`#!/usr/bin/env node`))
         .pipe(chmod(0o755))
         .pipe(gulp.dest('dist'))
 }
